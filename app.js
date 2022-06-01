1bc7c27e5a2dcf4f518741b462826ab37f418b04
Pauhoa <hoarau.paul3@gmail.com>
2022-06-01 13:18:12 +0400

    errors managed

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+const sequelize = require("./src/db/sequelize");
+const res = require("express/lib/response");
+
+const app = express();
+const port = 3000;
+
+app
+  .use(favicon(__dirname + "/favicon.ico"))
+  .use(morgan("dev"))
+  .use(bodyParser.json());
+
+sequelize.initDb();
+
+//  Ici, nous placerons nos endpoints
+require("./src/routes/findAllPokemons")(app);
+require("./src/routes/findPokemonByPk")(app);
+require("./src/routes/createPokemon")(app);
+require("./src/routes/updatePokemon")(app);
+require("./src/routes/deletePokemon")(app);
+
+// Ajout de gestion d'erreurs
+app.use(({ res }) => {
+  const message =
+    "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.";
+  res.status(404).json({ message });
+});
+
+app.listen(port, () =>
+  console.log(
+    `Notre application Node est démarrée sur : http://localhost:${port}`
+  )
+);
